9930bb658a03f875f9b21ac8fb5cabee3409eb5c
TeodoraDragieva <teodoradr.2018@gmail.com>
2024-10-15 22:19:19 +0300

    update

--- Loops_5/Ex5_Skip.java
+++ Loops_5/Ex5_Skip.java
@@ -1,12 +1,14 @@
-package Loops_5;
 
 public class Ex5_Skip {
 }
+public static void main(String[] args) {
+    int[] numbers = {12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200};
 
-
-
-
-
-
+    for (int num : numbers) {
+        if (num % 3 != 0) {
+            System.out.println("The number is: " + num);
+        }
+    }
+}
 
 //5. Print each number from an array of numbers by skipping numbers that multiples of 3
