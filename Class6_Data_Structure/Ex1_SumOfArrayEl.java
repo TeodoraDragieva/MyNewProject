ba74bb5274f49046a897d57b7f3813356ae0297c
TeodoraDragieva <teodoradr.2018@gmail.com>
2025-02-16 17:22:14 +0200

    Update Ex1_SumOfArrayEl.java

--- Class6_Data_Structure/Ex1_SumOfArrayEl.java
+++ Class6_Data_Structure/Ex1_SumOfArrayEl.java
@@ -8,14 +8,9 @@ public class Ex1_SumOfArrayEl {
         int[] numbers = {12, 15, 20};
         int sumOfElements = 0;
 
-        // Използване на цикъл for за сумиране на елементите
         for (int i = 0; i < numbers.length; i++) {
-            sumOfElements += numbers[i]; // Добавяне на елементите към сумата
+            sumOfElements += numbers[i]; 
         }
-
-        // Използване на Arrays.stream за сумиране (опционално, като алтернатива)
-        // int sumOfElements = Arrays.stream(numbers).sum();
-
         System.out.println("The Sum of elements is: " + sumOfElements);
     }
 }
