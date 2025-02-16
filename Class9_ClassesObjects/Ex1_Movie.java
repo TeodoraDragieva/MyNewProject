44ec644bc1c81080e94b4a65afebe28c43c4f731
TeodoraDragieva <teodoradr.2018@gmail.com>
2025-02-16 17:19:04 +0200

    Update Ex1_Movie.java

--- Class9_ClassesObjects/Ex1_Movie.java
+++ Class9_ClassesObjects/Ex1_Movie.java
@@ -1,17 +1,14 @@
 class Movie {
-    // Attributes of the Movie class
     private String title;
     private String genre;
     private double rating;
 
-    // Constructor to initialize the Movie object
     public Movie(String title, String genre, double rating) {
         this.title = title;
         this.genre = genre;
         this.rating = rating;
     }
 
-    // Method to display the movie details
     public void displayMovieDetails() {
         System.out.println("Title: " + title);
         System.out.println("Genre: " + genre);
@@ -22,12 +19,10 @@ class Movie {
 
 class MovieApp {
     public static void main(String[] args) {
-        // Creating movie objects
         Movie movie1 = new Movie("Inception", "Sci-Fi", 8.8);
         Movie movie2 = new Movie("The Godfather", "Crime", 9.2);
         Movie movie3 = new Movie("Interstellar", "Adventure", 8.6);
 
-        // Displaying movie details
         movie1.displayMovieDetails();
         movie2.displayMovieDetails();
         movie3.displayMovieDetails();
