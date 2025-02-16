767f78ca15d6f8594f757bae38a499b827142c53
TeodoraDragieva <teodoradr.2018@gmail.com>
2025-02-16 17:20:38 +0200

    Update Ex3_AgeCheck.java

--- Class5_Operators_Statements/Ex3_AgeCheck.java
+++ Class5_Operators_Statements/Ex3_AgeCheck.java
@@ -3,11 +3,6 @@ import java.util.Scanner;
 
 public class Ex3_AgeCheck {
 }
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 
     public static void main(String[] args) {
     System.out.print("Enter your age: ");
