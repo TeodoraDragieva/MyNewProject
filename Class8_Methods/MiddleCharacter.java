ec57f6dfa512f2ad78c0deb274c71c167d9d33b8
TeodoraDragieva <teodoradr.2018@gmail.com>
2025-02-16 17:17:58 +0200

    Update MiddleCharacter.java

--- Class8_Methods/MiddleCharacter.java
+++ Class8_Methods/MiddleCharacter.java
@@ -4,16 +4,14 @@ public class MiddleCharacter {
         String str = "example";
         System.out.println("The middle character(s): " + getMiddleCharacter(str));
     }
-
-    // Метод за извеждане на средния(ите) символ(и) на низ
     public static String getMiddleCharacter(String str) {
         int length = str.length();
         int middle = length / 2;
 
-        if (length % 2 == 0) {  // Проверка за четен брой символи
-            return str.substring(middle - 1, middle + 1); // Връща два символа
-        } else {  // Проверка за нечетен брой символи
-            return String.valueOf(str.charAt(middle)); // Връща един символ
+        if (length % 2 == 0) {  
+            return str.substring(middle - 1, middle + 1); 
+        } else { 
+            return String.valueOf(str.charAt(middle)); 
         }
     }
 }
